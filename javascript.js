4c99856144b02a97ba99ea75905f423897ed0663
Bluesman98 <papaderos1998@gmail.com>
2022-05-19 13:15:05 +0300

    make all messages previously shown to log appear in the page via DOM and add html elements to contain the score

--- javascript.js
+++ javascript.js
@@ -1,4 +1,5 @@
 
+
 function computerPlay() {
     let x = Math.floor(Math.random()*3+1);
     if (x==1) return "rock";
@@ -10,45 +11,46 @@ function computerPlay() {
 
 
 function playRound(playerSelection, computerSelection) {
-    console.log("Player's pick: ",playerSelection);
-    console.log("Computer's pick :" ,computerSelection);
+    const msg = document.querySelector('.msg');
+    const computerPlay = document.querySelector('.computer-play');
+    computerPlay.textContent = `Computer plays: ${computerSelection}` ;
     let result;
     if (playerSelection!=computerSelection) {
             if (playerSelection=="rock") {
                 if (computerSelection=="paper") {
-                    console.log("You loose, paper beats rock");
+                    msg.textContent = "You loose, paper beats rock";
                     return result = 0 ;    
                 }
                 else if (computerSelection=="scissors") {
-                    console.log("You win, rock beats scissors");
+                    msg.textContent = "You win, rock beats scissors";
                     return result = 1 ;
                 }
             }
             else if (playerSelection=="paper") {
                 if (computerSelection=="scissors") {
-                    console.log("You loose, scissors beats paper");
+                    msg.textContent = "You loose, scissors beats paper";
                     return result = 0 ;
                 }
                 else if (computerSelection=="rock") {
-                     console.log("You win, paper beats rock");
+                     msg.textContent = "You win, paper beats rock";
                      return result = 1 ;
                 }
             }
 
             else {
                 if (computerSelection=="rock") {
-                    console.log("You loose, rock beats scissors");
+                    msg.textContent = "You loose, rock beats scissors";
                     return result = 0 ;
                 }
                 else if (computerSelection=="paper") {
-                    console.log("You win, scissors beats paper");
+                    msg.textContent = "You win, scissors beats paper";
                     return result = 1 ;
                 }
             }
     }
 
     else {
-    console.log("It's a tie");
+    msg.textContent = "It's a tie";
     return 0;
     }
 }
@@ -60,12 +62,12 @@ function game(rounds) {
         score += playRound(playerplay,computerPlay());
     }
 
-    console.log("final score is :");
-    console.log("Player : ",score);
-    console.log("Compuer : ",rounds-score);
+    //msg.textContent("final score is :");//
+   // msg.textContent("Player : ",score);//
+    //msg.textContent("Compuer : ",rounds-score);//
 
-    if (score > rounds/2) return console.log ("Player wins the game");
-    else return console.log ("Computer wins the game");
+    //if (score > rounds/2) return msg.textContent ("Player wins the game");
+    //else return msg.textContent ("Computer wins the game");//
   }
 
 function playerInput(){
@@ -73,7 +75,7 @@ function playerInput(){
         let input = prompt("Rock, Paper or Scissors ? ");
         input = input.toLowerCase();
         if (input == "rock" || input== "paper" || input == "scissors") {
-         console.log(input)  ; 
+         msg.textContent(input)  ; 
          return input; 
         }
     }
@@ -82,5 +84,5 @@ function playerInput(){
 const btn = document.querySelectorAll('.btn');
 
 btn.forEach(e => e.addEventListener('click', event => {
-  let play = playRound(e.id,computerPlay());
+  let play = playRound(e.id, computerPlay());
 }));
